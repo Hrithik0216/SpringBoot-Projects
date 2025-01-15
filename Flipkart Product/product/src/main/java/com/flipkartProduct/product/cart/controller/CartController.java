4f8c58f0bbaceda42934d51a14fa5e4e21d32439
hrithik+1-at-970287672632 <hrithik@salesgear.io>
2025-01-15 12:39:30 +0530

    Add to cart api is validated

--- Flipkart Product/product/src/main/java/com/flipkartProduct/product/cart/controller/CartController.java
+++ Flipkart Product/product/src/main/java/com/flipkartProduct/product/cart/controller/CartController.java
@@ -8,8 +8,10 @@ import com.flipkartProduct.product.cart.service.CartService;
 import com.flipkartProduct.product.members.model.User;
 import com.flipkartProduct.product.members.repository.MemberRepository;
 import com.flipkartProduct.product.members.repository.MemberRepositoryCustom;
+import com.flipkartProduct.product.utils.JwtUtils;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.mongodb.core.MongoTemplate;
@@ -17,10 +19,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
+import java.util.logging.Logger;
 
 @RestController
 @RequestMapping("/cart")
 public class CartController {
+
+    private static final Logger LOGGER = Logger.getLogger(CartController.class.getName());
 
     @Autowired
     private CartService cartService;
@@ -45,45 +50,68 @@ public class CartController {
      * */
     @PostMapping("/addToCart")
     public ResponseEntity<ApiResponse<Cart>> addToCart(HttpServletRequest request, HttpServletResponse response, @RequestBody Cart newCart) {
-        String userId = request.getHeader("userId");
-        String role = request.getHeader("role");
-        User user = memberRepository.findByID(userId, role);
+        String authorization = request.getHeader("Authorization");
+        if (authorization != null && authorization.startsWith("Bearer ")) {
+            String token = authorization.substring(7);
+            try {
+                String userId = JwtUtils.validateJwtTokenAndGetUserId(token);
+                boolean userRole = JwtUtils.validateJwtTokenAndGetRoles(token);
+                System.out.println("userRole: " + userRole);
+                User user = memberRepository.findByUserId(userId);
+                if (user != null) {
+                    Optional<Cart> existingCartCheckWithUserId = Optional.ofNullable(cartRepository.findCartByUserId(userId));
 
-        if (user != null) {
-            Optional<Cart> existingCartCheckWithUserId = Optional.ofNullable(cartRepository.findCartByUserId(userId));
-
-            if (existingCartCheckWithUserId.isPresent()) {
-                Cart existingCart = existingCartCheckWithUserId.get();
-                Cart updatedCart = cartService.addToExistingCartOfExistingUser(
-                        userId,
-                        existingCart.getProductList(),
-                        newCart.getProductList()
-                );
-                updatedCart.setId(existingCart.getId());
-                cartRepository.save(updatedCart);
-                ApiResponse<Cart> apiResponseBody = new ApiResponse<>("Added to an existing cart", updatedCart);;
-                return ResponseEntity.ok(apiResponseBody);
-            } else {
-                newCart.setUser(userId); // Assuming `setUserId` is the correct method
-                cartRepository.save(newCart);
-                ApiResponse<Cart> apiResponseBody = new ApiResponse<>("Added to a new cart", newCart);
-                return ResponseEntity.ok(apiResponseBody);
+                    if (existingCartCheckWithUserId.isPresent()) {
+                        Cart existingCart = existingCartCheckWithUserId.get();
+                        Cart updatedCart = cartService.addToExistingCartOfExistingUser(
+                                userId,
+                                existingCart.getProductList(),
+                                newCart.getProductList()
+                        );
+                        updatedCart.setId(existingCart.getId());
+                        cartRepository.save(updatedCart);
+                        ApiResponse<Cart> apiResponseBody = new ApiResponse<>("Added to an existing cart", updatedCart);
+                        ;
+                        return ResponseEntity.ok(apiResponseBody);
+                    } else {
+                        newCart.setUser(userId); // Assuming `setUserId` is the correct method
+                        cartRepository.save(newCart);
+                        ApiResponse<Cart> apiResponseBody = new ApiResponse<>("Added to a new cart", newCart);
+                        return ResponseEntity.ok(apiResponseBody);
+                    }
+                } else {
+                    return ResponseEntity.status(HttpServletResponse.SC_UNAUTHORIZED).build();
+                }
+            } catch (Exception e) {
+                return ResponseEntity.status(HttpServletResponse.SC_UNAUTHORIZED).build();
             }
-        } else {
-            return ResponseEntity.status(HttpServletResponse.SC_UNAUTHORIZED).build();
         }
+        LOGGER.info("Auhtorization token is not provided");
+        return ResponseEntity.status(HttpServletResponse.SC_UNAUTHORIZED).build();
     }
 
-    @GetMapping("getMyCart/{userId}")
-    public ResponseEntity<CartDto> getCart(@PathVariable("userId") String userId) {
-        Optional<User> user = Optional.of(secondaryMongoTemplate.findById(userId, User.class));
-        if (user.isPresent()) {
-            System.out.println("Found the user");
-            return cartService.getCart(userId);
-        } else {
-            return ResponseEntity.badRequest().build();
+    @GetMapping("/getMyCart")
+    public ResponseEntity<CartDto> getCart(HttpServletRequest request) {
+        String authorization = request.getHeader("Authorization");
+
+        if (authorization != null && authorization.startsWith("Bearer ")) {
+            String token = authorization.substring(7);
+            String userId = JwtUtils.validateJwtTokenAndGetUserId(token);
+            boolean userRole = JwtUtils.validateJwtTokenAndGetRoles(token);
+            if (userRole) {
+                try {
+                    Optional<User> user = Optional.of(secondaryMongoTemplate.findById(userId, User.class));
+                    if (user.isPresent()) {
+                        return cartService.getCart(userId);
+                    } else {
+                        return ResponseEntity.badRequest().build();
+                    }
+                } catch (Exception e) {
+                    LOGGER.info("The err is" + e);
+                }
+            }
         }
+        LOGGER.info("Authorization is not provide to get cartdetails");
+        return ResponseEntity.status(HttpServletResponse.SC_UNAUTHORIZED).build();
     }
-
-
 }
