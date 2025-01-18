85a097807069866f635d56349004e296717b58cd
hrithik+1-at-970287672632 <hrithik@salesgear.io>
2025-01-18 17:16:21 +0530

    Add and get from cart is done

--- Flipkart Product/product/src/main/java/com/flipkartProduct/product/cart/repository/CartRepositoryCustom.java
+++ Flipkart Product/product/src/main/java/com/flipkartProduct/product/cart/repository/CartRepositoryCustom.java
@@ -1,10 +1,7 @@
 package com.flipkartProduct.product.cart.repository;
 
-import com.flipkartProduct.product.cart.DTO.CartDto;
 import com.flipkartProduct.product.cart.model.Cart;
 import org.springframework.http.ResponseEntity;
-
-import java.util.List;
 
 public interface CartRepositoryCustom {
     ResponseEntity<?> findCartByCustomerId(String customerId);
